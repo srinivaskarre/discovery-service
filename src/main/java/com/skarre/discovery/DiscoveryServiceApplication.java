64c146c6e97caf3daafdea52dc4488ee31616d3b
srinivas <srinivaskarre@rocketmail.com>
2020-01-05 15:51:43 +0530

    Lombok addition

--- src/main/java/com/skarre/discovery/DiscoveryServiceApplication.java
+++ src/main/java/com/skarre/discovery/DiscoveryServiceApplication.java
@@ -1,14 +1,17 @@
 package com.skarre.discovery;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@Slf4j
 public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
+		log.info("Before Start");
 		SpringApplication.run(DiscoveryServiceApplication.class, args);
 	}
 
